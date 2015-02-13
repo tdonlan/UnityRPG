ca21296462b4ec11adbe6c01bbbeef73b58fa4ea
tdonlan <timdonlan@gmail.com>
2015-02-13 16:21:02 -0500

    Added a start scene wrapper for game

---
+++ Assets/Scripts/StartGameScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGameScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadBattle()
+    {
+        Application.LoadLevel("BattleScene");
+    }
+}
