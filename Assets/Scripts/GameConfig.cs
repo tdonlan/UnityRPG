74a331c449aa966bdd48c0410110258f4b7fced0
tdonlan <timdonlan@gmail.com>
2015-02-26 23:12:37 -0500

    Player buttons enable/disable on click

---
+++ Assets/Scripts/GameConfig.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+using System;
+using UnityEngine.UI;
+using Assets;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+using System.Linq;
+
+
+
+    public class GameConfig
+    {
+
+        public static readonly Vector3 AbilityPanelDisplayLocation = new Vector3(330, -250, -1);
+        public static readonly Vector3 ItemPanelDisplayLocation = new Vector3(330, -250, -1);
+        public static readonly Vector3 EquipPanelDisplayLocation = new Vector3(0, 0, 0);
+
+        public static readonly Vector3 AbilityPanelHideLocation = new Vector3(300, -600, 0);
+        public static readonly Vector3 ItemPanelHideLocation = new Vector3(300, -600, 0);
+        public static readonly Vector3 EquipPanelHideLocation = new Vector3(-1400, -0, 0);
+
+
+    }
+
