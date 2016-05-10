2c1ae2d00f5134abd237081a4c54d57c7bed9751
Tim <timdonlan@gmail.com>
2016-05-09 20:20:25 -0400

    Added InventoryTest scene

---
+++ Assets/Scripts/SceneControllers/InventoryTestController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InventoryTestController : MonoBehaviour
+{
+	public GameDataObject gameDataObject { get; set; }
+
+	public Text debugText;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+
+	void OnLevelWasLoaded(int level)
+	{
+		loadGameData();
+
+	}
+
+	private void loadGameData()
+	{
+		gameDataObject = GameObject.FindObjectOfType<GameDataObject>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
+
